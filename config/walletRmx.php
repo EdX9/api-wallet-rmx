9d8024247487a397791eb0b4d72b65bdba175555
lalo <emontano@bit.lat>
2022-06-07 17:31:29 -0500

    Creación de archivo de configuración Base de la Wallet

---
+++ config/walletRmx.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Wallet\Transaction;
+use App\Models\Wallet\Transfer;
+use App\Models\Wallet\Wallet;
+
+
+return [
+    'math' => [
+        'scale' => 64,
+    ],
+    /**
+     * Base model 'transaction'.
+     */
+    'transaction' => [
+        'table' => 'transactions',
+        'model' => Transaction::class,
+    ],
+
+    /**
+     * Base model 'transfer'.
+     */
+    'transfer' => [
+        'table' => 'transfers',
+        'model' => Transfer::class,
+    ],
+
+    /**
+     * Base model 'wallet'.
+     */
+    'wallet' => [
+        'table' => 'wallets',
+        'model' => Wallet::class,
+        'creating' => [],
+        'default' => [
+            'name' => 'Default Wallet',
+            'slug' => 'default',
+            'meta' => [],
+        ],
+    ],
+];

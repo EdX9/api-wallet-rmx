966ca7d876580b7397edd31b5751253e7c79951e
lalo <emontano@bit.lat>
2022-06-10 16:29:57 -0500

    Se Agrego el AtomicBalanceProvider

---
+++ app/Providers/AtomicBalanceUpdateProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Services\AtomicTransaction\AtomicBalanceUpdate;
+
+class AtomicBalanceUpdateProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(AtomicBalanceUpdate::class, function ($app) {
+            return new AtomicBalanceUpdate(
+                app(lockService::class),
+                app(MathService::class),
+                config()
+            );
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}

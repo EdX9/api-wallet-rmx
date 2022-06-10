31a7d0988ac349f7c7542c53d60b6ccffbf003ee
lalo <emontano@bit.lat>
2022-06-10 16:29:19 -0500

    Correccion a AtomicBlance y se agrego el método de transferencia

--- app/Services/AtomicTransaction/AtomicBalanceUpdate.php
+++ app/Services/AtomicTransaction/AtomicBalanceUpdate.php
@@ -5,25 +5,29 @@ namespace App\Services\AtomicTransaction;
 use Exception;
 use App\Models\Wallet\Wallet;
 use App\Models\Wallet\Transaction;
+use App\Models\Wallet\Transfer;
 use App\Services\Lock\lockService;
 use App\Services\Math\MathService;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Config\Repository as ConfigRepository;
 
 class AtomicBalanceUpdate
 {
     protected $lastTransaction;
-    private $ttlBlock = 3;
+    private $ttlBlock;
     private $decimalPlacesWallet;
     private $decimalPlacesAdjustment;
+    private $wallet;
+    private $walletClass;
 
     public function __construct(
         private lockService $cache,
         private MathService $math,
-        private Wallet $wallet
+        private ConfigRepository $config
         ) 
     {
-        $this->decimalPlacesWallet = $this->wallet->decimal_places;
-        $this->decimalPlacesAdjustment = $math->powTen($this->decimalPlacesWallet);
+        $this->walletClass = $this->config->get('walletRmx.wallet.model',  Wallet::class);
+        $this->ttlBlock = $this->config->get('walletRmx.lock.seconds', 3);
     }
 
     public function setTtlBlock(int $ttlBlock)
@@ -31,26 +35,44 @@ class AtomicBalanceUpdate
         $this->ttlBlock = $ttlBlock;
     }
 
+    private function verifyWallet()
+    {
+        if (!$this->wallet instanceof $this->walletClass) {
+            throw new Exception('Error Wallet no valida');
+        }
+    }
+
+    public function setWallet($wallet)
+    {
+        $this->wallet = $wallet;
+        $this->verifyWallet();
+        $this->decimalPlacesWallet = $this->wallet->decimal_places;
+        $this->decimalPlacesAdjustment = $this->math->powTen($this->decimalPlacesWallet);
+        return $this;
+    }
+
     public function deposit(string $amount,array $meta = [],bool $confirmed = true)
     {
-        
+        $this->verifyWallet();
+        $amount=$this->getBalanceToDecimal($amount);
         return $this->cache->lock('wallet:update:balance:lock',$this->ttlBlock,function() use ($amount,$meta,$confirmed){
-            DB::transaction(function () use ($amount,$meta,$confirmed) { // Start the transaction
-                $this->wallet->balance = $this->getBalanceToDecimal(
-                    $this->math->add(
+            // Start the transaction
+            DB::transaction(function () use ($amount,$meta,$confirmed) { 
+                $this->wallet->balance = $this->math->add(
                         $this->wallet->balance,$amount,
-                        $this->wallet->decimal_places));   
+                        $this->wallet->decimal_places);
                 $this->wallet->save();
                 $this->setTransaction(Transaction::TYPE_DEPOSIT,$amount,$confirmed,$meta);
                 
-            }); // End transaction
-            
+            }); 
+            // End transaction
             return $this->wallet;
          });
     }
 
     public function withdraw(string $amount,array $meta = [],bool $confirmed = true,bool $force=false)
     {
+        $this->verifyWallet();
         $amount=$this->getBalanceToDecimal($amount);
         return $this->cache->lock('wallet:update:balance:lock',$this->ttlBlock,function() use ($amount,$meta,$confirmed,$force)
         {
@@ -60,17 +82,43 @@ class AtomicBalanceUpdate
                     throw new Exception('Fondos Insuficientes');
                 }
             }
-            DB::transaction(function () use ($amount,$meta,$confirmed) { // Start the transaction
+            // Start the transaction
+            DB::transaction(function () use ($amount,$meta,$confirmed) { 
                 $this->wallet->balance = 
                     $this->math->sub(
                         $this->wallet->balance,
                         $amount,
                         $this->wallet->decimal_places);    
                 $this->wallet->save();
+                $amount = $this->math->negative($amount);
                 $this->setTransaction(Transaction::TYPE_WITHDRAW,$amount,$confirmed,$meta);
-            }); // End transaction
+            }); 
+            // End transaction
             return $this->wallet;
          });
+    }
+
+    public function transfer($walletReceiver,string $amount,array $meta = [],bool $force=false)
+    {
+        // Start the transaction
+        DB::transaction(function () use ($amount,$walletReceiver,$meta,$force) { 
+            $originalWallet = $this->wallet;
+            \Log::debug("OriginalWallet:".$originalWallet->id);
+            $this->withdraw($amount,$meta,true,$force);
+            $withdraws = $this->getLastTransaction()[0];
+
+            $this->setWallet($walletReceiver);
+            \Log::debug("newWallet:".$this->wallet->id.'  '.$walletReceiver->id);
+
+            $this->deposit($amount,$meta,true);
+            $deposit = $this->getLastTransaction()[0];
+            $this->setWallet($originalWallet);
+            \Log::debug("lastWallet:".$this->wallet->id);
+            $this->setTransfer($originalWallet, $walletReceiver,Transfer::STATUS_TRANSFER,$deposit,$withdraws);
+
+        }); 
+        // End transaction
+        return $this->wallet;
     }
 
     private function getBalanceToDecimal(string $balance)
@@ -89,6 +137,22 @@ class AtomicBalanceUpdate
                 'amount' => $amount,
                 'confirmed' => $confirmed,
                 'meta'=>$meta,
+            ]
+        );
+    }
+
+    private function setTransfer($wallet, $walletReceiver,string $type,$deposit,$withdraws)
+    {
+        
+        $this->lastTransaction = Transfer::create(
+            [
+                'from_type' => $this->walletClass,
+                'from_id' => $wallet->id,
+                'to_type' => $this->walletClass,
+                'to_id' => $walletReceiver->id,
+                'status' => $type,
+                'deposit_id' => $deposit->id,
+                'withdraw_id' => $withdraws->id,
             ]
         );
     }

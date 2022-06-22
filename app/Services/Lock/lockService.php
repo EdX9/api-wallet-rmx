7c8ebe07caa7c561f14a9a59469a36d019606060
lalo <emontano@bit.lat>
2022-06-22 13:38:05 -0500

    Se elimino el código de prueba

--- app/Services/Lock/lockService.php
+++ app/Services/Lock/lockService.php
@@ -3,21 +3,11 @@
 namespace App\Services\Lock;
 
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Redis;
 
 class lockService
 {
     public function __construct() {
         $this->Cache = new Cache();
-    }
-    /**
-     * ELIMINAR
-     */
-    public function test()
-    {
-        Redis::set('name', 'Jack');
-        Cache::store('redis')->put('addTestR', 'valueR');
-        Cache::add('addTest', 'value', 1*60);
     }
 
     public function lock($key,$ttl,$Closure)
